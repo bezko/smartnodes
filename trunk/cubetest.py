03971252d5334a4b4d8a6639bb45d698e61163f5
bezkom <bezkom@fae5ff54-fec4-11dd-8c4e-4bbb75a408b7>
2009-02-20 21:42:59 +0000

    I thought I committed this

--- trunk/cubetest.py
+++ trunk/cubetest.py
@@ -4,7 +4,8 @@ import math
 cube = SmartNode("cube")
 sinusx = SmartSinus(1,amp=2)
 sinusz = SmartSinus(1,amp=2,phase=math.pi/2)
-randiy = SmartRandi(2,amp=4)
+randiy = SmartRandi(8,amp=.5,offset=1)
 cube.tx = sinusx.out[0]
 cube.tz = sinusz.out[0]
 cube.sy = randiy.out[0]
+cube.sz=cube.sx = 1/randiy.out[0]
